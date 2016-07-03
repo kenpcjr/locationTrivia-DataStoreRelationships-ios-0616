9730a1d3313fa17bc0b8ce52989b1438c5e2dc90
kenpcjr <kennethcooke@me.com>
2016-07-03 14:26:30 -0400

    adds save to data store

---
+++ locationTrivia-dataStore/FISAddLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISAddLocationViewController.h
+//  locationTrivia-dataStore
+//
+//  Created by Kenneth Cooke on 7/3/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAddLocationViewController : UIViewController
+
+@end
